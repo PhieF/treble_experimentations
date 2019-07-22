c319ce59b62f4ffee862e5cdae862d8d4695f984
Phie <phie@phie.ovh>
2019-07-22 12:22:50 +0200

    this revert is in patches

--- build-dakkar.sh
+++ build-dakkar.sh
@@ -342,8 +342,6 @@ function patch_things() {
             cd device/phh/treble
             git revert 82b15278bad816632dcaeaed623b569978e9840d -n
             git revert df25576594f684ed35610b7cc1db2b72bc1fc4d6 -n
-            cd ../../../build/make
-            git revert b9acd8b4f49d710b4dae2c73ac8e919589f76d44 -n
             cd ../../external/tinycompress
             git revert fbe2bd5c3d670234c3c92f875986acc148e6d792 -n
             cd ../../vendor/qcom/opensource/cryptfs_hw
